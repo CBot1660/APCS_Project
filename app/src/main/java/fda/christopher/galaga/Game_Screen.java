2b05ede64129239f7ba2527c02e328dd77c75830
CBot <christopherrodriguezfda@gmail.com>
2017-05-22 16:00:05 -0400

    Base for homescreen and gamescreen

--- app/src/main/java/fda/christopher/galaga/Game_Screen.java
+++ app/src/main/java/fda/christopher/galaga/Game_Screen.java
@@ -2,6 +2,7 @@ package fda.christopher.galaga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Game_Screen extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class Game_Screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game__screen);
         ImageView character = (ImageView) findViewById(R.id.imageView);
-
+        Button upButton = (Button) findViewById(R.id.upButton);
+        Button downButton = (Button) findViewById(R.id.downButton);
+        Button leftButton = (Button) findViewById(R.id.leftButton);
+        Button rightButton = (Button) findViewById(R.id.rightButton);
     }
 }
